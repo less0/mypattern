4d8cb4d4e81ac77a91ce0bd6f7d76c59984a1898
less0 <p.k@pajap.eu>
2014-01-30 06:29:40 +0000

    Added XmlElement to mypattern-data to represent and parse single XML-tags implemented parsing of tags in XmlElement smaller bugfixes in XmlParameter implemented tests for XmlElement and XmlParameter

---
+++ mypattern-data/include/xmlelement.h
@@ -0,0 +1,68 @@
+#ifndef MYPATTERN_DATA_XMLELEMENT_H
+#define MYPATTERN_DATA_XMLELEMENT_H
+
+#include "exception.h"
+#include "glibmm/ustring.h"
+#include "xmlparameter.h"
+#include <memory>
+#include <list>
+
+//using namespace MyPattern::Base;
+using namespace MyPattern::Exceptions;
+
+
+namespace MyPattern {
+namespace Data {
+
+class XmlElement
+{
+    public:
+        /** Default constructor */
+        XmlElement();
+
+        XmlElement(Glib::ustring name, list<XmlParameter> attributes, bool isEmpty, bool isEndElement);
+        /** Default destructor */
+        virtual ~XmlElement();
+        /** Copy constructor
+         *  \param other Object to copy from
+         */
+        XmlElement(const XmlElement& other);
+        /** Assignment operator
+         *  \param other Object to assign from
+         *  \return A reference to this
+         */
+        XmlElement& operator=(const XmlElement& other);
+        /** Access m_Attributes;
+         * \return The current value of m_Attributes;
+         */
+        list<MyPattern::Data::XmlParameter> GetAttributes() { return m_Attributes; }
+
+        void AddAttribute(XmlParameter attribute);
+        /** Access m_isEmpty
+         * \return The current value of m_isEmpty
+         */
+        bool isEmpty() { return m_isEmpty; }
+
+        bool isEndElement() { return m_isEndElement; }
+
+        /*! \brief Gets the name of the element */
+        Glib::ustring GetName()
+        {
+            return m_name;
+        }
+
+        static XmlElement parse_element(Glib::ustring schema, int start_index, int& end_index);
+    protected:
+    private:
+        list<XmlParameter> m_Attributes; //!< Member variable "m_Attributes;"
+        bool m_isEmpty; //!< Member variable "m_isEmpty"
+        bool m_isEndElement;
+        Glib::ustring m_name;
+
+        static bool is_valid_name_char(char c);
+};
+
+} // namespace Data
+} // namespace MyPattern
+
+#endif // MYPATTERN_DATA_XMLELEMENT_H
