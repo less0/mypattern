c4a54881b2eada31fad9ed249902771e830f91e5
paul <paul@498744f8-7161-430b-abba-7d1c151b1f15>
2014-02-12 16:46:14 +0000

    Formula evaluation * Added tests for formulae which have a negative first number (also within parentheses) * Implemented workaround to handle these formulae

--- trunk/mypattern-base-test/src/FormulaTest.cpp
+++ trunk/mypattern-base-test/src/FormulaTest.cpp
@@ -133,19 +133,50 @@ namespace
 		map<ustring,double> values;
 		CHECK_CLOSE(123.0*3, t->evaluate(values), 1e-6);
 	}
-	
+
 	TEST(TestLiteralFormulaWithParenteses2)
 	{
 		shared_ptr<Term> t = Term::parse("22/(2+2)");
 		map<ustring,double> values;
 		CHECK_CLOSE(5.5, t->evaluate(values), 1e-6);
 	}
-	
+
 	TEST(TestLiteralFormulaWithNestedDivisions)
 	{
 		shared_ptr<Term> t = Term::parse("1/(1/2)");
 		map<ustring,double> values;
-		
+
 		CHECK_CLOSE(2.0, t->evaluate(values), 1e-6);
 	}
+
+	TEST(TestSymbolicFormulaWithNestedDivision)
+	{
+        shared_ptr<Term> t = Term::parse("#foo/(#bar/#foobar)");
+        map<ustring, double> values;
+        values.insert(pair<ustring, double>("#foo", 3.141));
+        values.insert(pair<ustring, double>("#bar", 1));
+        values.insert(pair<ustring, double>("#foobar", 3.141));
+
+        CHECK_CLOSE(3.141*3.141, t->evaluate(values), 1e-6);
+	}
+
+    TEST(TestFormulaWithPrecedingMinus)
+    {
+        shared_ptr<Term> t = Term::parse("-#foo+#bar");
+        map<Glib::ustring,double> values;
+        values.insert(pair<Glib::ustring,double>("#foo", 1.23));
+        values.insert(pair<Glib::ustring,double>("#bar", 3.21));
+        CHECK_CLOSE(-1.23+3.21, t->evaluate(values), 1e-6);
+    }
+
+    TEST(TestParenthesesWithPrecedingMinus)
+    {
+        shared_ptr<Term> t = Term::parse("#foobar*(-#foo+#bar)");
+        map<Glib::ustring,double> values;
+        values.insert(pair<Glib::ustring,double>("#foo", 1.23));
+        values.insert(pair<Glib::ustring,double>("#bar", 3.21));
+        values.insert(pair<Glib::ustring,double>("#foobar", 123.4));
+
+        CHECK_CLOSE(123.4*(-1.23+3.21), t->evaluate(values), 1e-6);
+    }
 }
