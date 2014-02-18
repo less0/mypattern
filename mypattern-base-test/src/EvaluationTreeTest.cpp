bcbf22c7132dd5328b215ca2a26e755e26d481e2
less0 <p.k@pajap.eu>
2014-02-18 16:38:06 +0000

    Implementation of TreeEvaluation * added LandmarkEvaluationTreeNode, ArgumentNullException * Modifications in Makefiles * Imeplementation in EvaluationTreeTest and EvaluationRoot

--- mypattern-base-test/src/EvaluationTreeTest.cpp
+++ mypattern-base-test/src/EvaluationTreeTest.cpp
@@ -3,15 +3,18 @@
 #include "patternobject.h"
 #include "landmark.h"
 
+using namespace MyPattern::Base;
+using namespace MyPattern::Base::Evaluation;
+
 namespace
 {
     TEST(AddLandmarkToTree)
     {
-//        EvaluationRoot root = EvaluationRoot();
-//        shared_ptr<PatternObject> landmark = shared_ptr<PatternObject>(new Landmark());
-//
-//
-//
-//        root.add_object(shared_ptr<PatternObject>());
+        EvaluationRoot root = EvaluationRoot();
+        shared_ptr<PatternObject> landmark = shared_ptr<PatternObject>(new Landmark());
+
+
+        shared_ptr<EvaluationTreeNode> node =  root.add_object(landmark);
+        CHECK_EQUAL(true, node != NULL);
     }
 }
--- mypattern-base/src/evaluation/evaluationroot.cpp
+++ mypattern-base/src/evaluation/evaluationroot.cpp
@@ -1,13 +1,31 @@
 #include "evaluation/evaluationroot.h"
+#include "landmark.h"
+#include "evaluation/landmarkevaluationtreenode.h"
 
 namespace MyPattern {
 namespace Base {
 namespace Evaluation {
 
-EvaluationRoot::EvaluationRoot()
-{
+    EvaluationRoot::EvaluationRoot()
+    {
 
-}
+    }
+
+    shared_ptr<EvaluationTreeNode> EvaluationRoot::add_object(shared_ptr<PatternObject> object)
+    {
+
+
+
+        shared_ptr<Landmark> p_landmark = dynamic_pointer_cast<Landmark>(object);
+
+
+        if(p_landmark != NULL)
+        {
+            return shared_ptr<EvaluationTreeNode>(new LandmarkEvaluationTreeNode());
+        }
+
+        return shared_ptr<EvaluationTreeNode>(NULL);
+    }
 
 } // namespace Evaluation
 } // namespace Base
