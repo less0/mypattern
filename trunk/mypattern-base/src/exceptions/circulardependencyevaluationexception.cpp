329e32a300262e0faf40e898ca443820e306997d
paul <paul@498744f8-7161-430b-abba-7d1c151b1f15>
2014-02-24 16:38:27 +0000

    Added CircularDependencyEvaluationException and modified Makefile in mypattern-base as well as mypattern-base.h

---
+++ trunk/mypattern-base/src/exceptions/circulardependencyevaluationexception.cpp
@@ -0,0 +1,29 @@
+#include "exceptions/circulardependencyevaluationexception.h"
+
+namespace MyPattern {
+namespace Exceptions {
+
+CircularDependencyEvaluationException::CircularDependencyEvaluationException()
+{
+    //ctor
+}
+
+CircularDependencyEvaluationException::~CircularDependencyEvaluationException()
+{
+    //dtor
+}
+
+CircularDependencyEvaluationException::CircularDependencyEvaluationException(const CircularDependencyEvaluationException& other)
+{
+    //copy ctor
+}
+
+CircularDependencyEvaluationException& CircularDependencyEvaluationException::operator=(const CircularDependencyEvaluationException& rhs)
+{
+    if (this == &rhs) return *this; // handle self assignment
+    //assignment operator
+    return *this;
+}
+
+} // namespace Exceptions
+} // namespace MyPattern
