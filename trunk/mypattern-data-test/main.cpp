46e4a196ca782ec17169e076c2dac1559626c4ad
paul <paul@498744f8-7161-430b-abba-7d1c151b1f15>
2014-03-15 20:17:17 +0000

    Changed test projects to create JUnit style output

--- trunk/mypattern-data-test/main.cpp
+++ trunk/mypattern-data-test/main.cpp
@@ -1,9 +1,21 @@
+#include <fstream>
 #include <iostream>
 #include "UnitTest++.h"
+#include "JUnitStyleReporter.h"
 
 using namespace std;
 
-int main()
+int main(int argc, const char* argv[])
 {
+    if(argc > 1 && string(argv[1]) == "junit")
+    {
+        ofstream os;
+        os.open("test.xml");
+
+        UnitTest::JUnitStyleReporter reporter(os);
+        UnitTest::TestRunner runner(reporter);
+        return runner.RunTestsIf(UnitTest::Test::GetTestList(), NULL, UnitTest::True(), 0);
+    }
+
     return UnitTest::RunAllTests();
 }
