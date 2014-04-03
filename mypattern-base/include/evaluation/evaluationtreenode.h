2a8413931c861c5b91bb6d9f550857b07a591267
paul <paul@498744f8-7161-430b-abba-7d1c151b1f15>
2014-04-03 12:27:58 +0000

    Moved signals from LandmarkEvaluationTreeNode to base class EvaluationTreeNode

--- mypattern-base/include/evaluation/evaluationtreenode.h
+++ mypattern-base/include/evaluation/evaluationtreenode.h
@@ -63,6 +63,14 @@ class EvaluationTreeNode : public std::enable_shared_from_this<EvaluationTreeNod
         virtual list<ustring> depends_on() = 0;
 
         virtual ustring get_prefixed_name() = 0;
+		
+		/*! \brief Gets the signal that checks the validity of a dependency
+         * change */
+        sigc::signal2<bool, shared_ptr<EvaluationTreeNode>, list<ustring>> signal_request_change;
+
+        /*! \brief Returns the signal that is emitted when the object has been
+         * updated and the dependencies are to be updated by the EvaluationRoot */
+        sigc::signal1<void, shared_ptr<EvaluationTreeNode>> signal_update_dependencies;
     protected:
         list<shared_ptr<EvaluationTreeNode>> m_nodes;
         std::map<long,sigc::connection> m_signal_connections;
