89b3ed9c43d92a4df10b00327d16c741294e0f12
paul <paul@498744f8-7161-430b-abba-7d1c151b1f15>
2014-04-04 05:42:45 +0000

    Added signal_request_change and signal_update_dependencies to CurveEvaluationTreeNode

--- mypattern-base/include/evaluation/curveevaluationtreenode.h
+++ mypattern-base/include/evaluation/curveevaluationtreenode.h
@@ -24,6 +24,14 @@ class CurveEvaluationTreeNode : public MyPattern::Base::Evaluation::EvaluationTr
 	 * given the values of the dependencies
 	 */
 	BezierComplex get_value();
+	
+	/*! \brief Gets the signal that checks the validity of a dependency
+         * change */
+        sigc::signal2<bool, shared_ptr<EvaluationTreeNode>, list<ustring>> signal_request_change;
+
+        /*! \brief Returns the signal that is emitted when the object has been
+         * updated and the dependencies are to be updated by the EvaluationRoot */
+        sigc::signal1<void, shared_ptr<EvaluationTreeNode>> signal_update_dependencies;
     protected:
         void update_value() {}
     private:
