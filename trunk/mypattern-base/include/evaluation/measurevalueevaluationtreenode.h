ea920ab32c0e002ebcd71c4f78c8765c2e620000
paul <paul@498744f8-7161-430b-abba-7d1c151b1f15>
2014-05-13 05:53:31 +0000

    Added MeasureEvaluationTreeNode class

---
+++ trunk/mypattern-base/include/evaluation/measurevalueevaluationtreenode.h
@@ -0,0 +1,36 @@
+#ifndef MYPATTERN_EVALUATION_MEASUREVALUEEVALUATIONTREENODE_H
+#define MYPATTERN_EVALUATION_MEASUREVALUEEVALUATIONTREENODE_H
+
+#include <evaluation/evaluationtreenode.h>
+#include <measurevalue.h>
+#include <glibmm/ustring.h>
+#include <list>
+
+using namespace std;
+using namespace MyPattern::Base;
+
+namespace MyPattern {
+namespace Base {
+namespace Evaluation {
+
+class MeasureValueEvaluationTreeNode : public EvaluationTreeNode
+{
+    public:
+        MeasureValueEvaluationTreeNode(shared_ptr<MeasureValue>);
+
+        void notify_update() {}
+        list<ustring> depends_on()
+        {
+            return list<Glib::ustring>();
+        }
+        Glib::ustring get_prefixed_name() { return "";}
+    protected:
+    private:
+        shared_ptr<MeasureValue> m_base_measurevalue;
+};
+
+} // namespace Evaluation
+} // namespace Base
+} // namespace MyPattern
+
+#endif // MYPATTERN_EVALUATION_MEASUREVALUEEVALUATIONTREENODE_H
---
+++ trunk/mypattern-base/src/evaluation/measurevalueevaluationtreenode.cpp
@@ -0,0 +1,14 @@
+#include "evaluation/measurevalueevaluationtreenode.h"
+
+namespace MyPattern {
+namespace Base {
+namespace Evaluation {
+
+MeasureValueEvaluationTreeNode::MeasureValueEvaluationTreeNode(shared_ptr<MyPattern::Base::MeasureValue> base_value)
+{
+    //ctor
+}
+
+} // namespace Evaluation
+} // namespace Base
+} // namespace MyPattern
